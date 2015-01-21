9a04167fa9883266f657b9e4bdf03be6d549d22e
kunzmainali <kunzmainali@hotmail.com>
2015-01-21 10:05:32 -0600

    viewcontroller

---
+++ Whale/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Whale
+//
+//  Created by Alex White on 11/2/14.
+//  Copyright (c) 2014 Alex White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
